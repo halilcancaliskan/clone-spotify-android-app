51d469e0cdc8c36c9dea6d0c7abeb34954ebfefb
Martinh59650 <martin.hilaire@decathlon.com>
2023-06-22 15:20:31 +0200

    ajout navhost

--- app/src/main/java/com/example/efficomobile/MainActivity.kt
+++ app/src/main/java/com/example/efficomobile/MainActivity.kt
@@ -3,48 +3,16 @@ package com.example.efficomobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.runtime.remember
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilterChip
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
+import androidx.navigation.compose.rememberNavController
 import com.example.efficomobile.ui.theme.EfficomobileTheme
 import com.example.efficomobile.ui.theme.Screen.Home
-import com.example.efficomobile.ui.theme.Screen.Library
 import com.example.efficomobile.ui.theme.Screen.SignUp
 
 class MainActivity : ComponentActivity() {
@@ -56,7 +24,7 @@ class MainActivity : ComponentActivity() {
             NavHost(navController = navController, startDestination = "Home") {
                 composable("Home") {
                     Home(navController)
-                    Library()}
+                    }
                 composable("SignUp") { SignUp() }
             }
         }
--- app/src/main/java/com/example/efficomobile/ui/theme/Screen/Home.kt
+++ app/src/main/java/com/example/efficomobile/ui/theme/Screen/Home.kt
@@ -1,24 +1,9 @@
 package com.example.efficomobile.ui.theme.Screen
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
@@ -27,67 +12,74 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import com.example.efficomobile.MainActivity
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.efficomobile.R
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 
-fun Home(navController: NavHostController){
+fun Home(navController: NavHostController) {
+    val navController = rememberNavController()
     Scaffold(topBar = {
         TopAppBar(title = { Text(text = "Bonjour") }, actions = {
             IconButton(onClick = { /*TODO*/ }) {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_cloche),
-                    contentDescription = "History"
+                    contentDescription = "Alerts"
                 )
             }
             IconButton(onClick = { /*TODO*/ }) {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_history),
-                    contentDescription = "Settings"
+                    contentDescription = "History"
                 )
             }
             IconButton(onClick = { navController.navigate("SignUp") }) {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_user),
-                    contentDescription = "Settings"
+                    contentDescription = "SignUp"
                 )
             }
         })
     }, bottomBar = {
         NavigationBar() {
-            NavigationBarItem(selected = true, onClick = { /*TODO*/ }, icon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_home_24),
-                    contentDescription = "Home"
-                )
-            }, label = { Text(text = "Home") })
-            NavigationBarItem(selected = false, onClick = { /*TODO*/ }, icon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_search_24),
-                    contentDescription = "Search"
-                )
-            }, label = { Text(text = "Search") })
-            NavigationBarItem(selected = false, onClick = { /*TODO*/ }, icon = {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_view_headline_24),
-                    contentDescription = "Library"
-                )
-            }, label = { Text(text = "Library") })
+            NavigationBarItem(
+                selected = true,
+                onClick = { navController.navigate("Home") },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_home_24),
+                        contentDescription = "Home"
+                    )
+                },
+                label = { Text(text = "Home") })
+            NavigationBarItem(
+                selected = false,
+                onClick = { navController.navigate("Search") },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_search_24),
+                        contentDescription = "Search"
+                    )
+                },
+                label = { Text(text = "Search") })
+            NavigationBarItem(
+                selected = false,
+                onClick = { navController.navigate("Library") },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_view_headline_24),
+                        contentDescription = "Library"
+                    )
+                },
+                label = { Text(text = "Library") })
 
 
         }
@@ -96,7 +88,25 @@ fun Home(navController: NavHostController){
     }) {
         val genre = listOf("Musique", "Podcast", "Vidéo", "Radio", "Live")
 
-        Column(
+        NavHost(
+            navController = navController,
+            startDestination = "Home",
+            modifier = Modifier
+                .padding(it)
+                .background(Color.Yellow)
+        )
+        {
+            composable("Home") {
+                Text(text = "je suis la home ")
+            }
+            composable("Search") {
+                Text(text = "je suis la search")
+            }
+            composable("Library") {
+                Text(text = "je suis la library")
+            }
+        }
+        /*Column(
             modifier = Modifier
                 .padding(it)
                 .verticalScroll(rememberScrollState())
@@ -324,6 +334,6 @@ fun Home(navController: NavHostController){
                     )
                 }
             }
-        }
+        }*/
     }
 }
