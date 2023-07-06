bd3c76d8377695e4ebb0e307c7d036c617ab8668
Halil Can CALISKAN <61836866+halilcancaliskan@users.noreply.github.com>
2023-07-06 17:06:38 +0200

    add new maj module graddle

--- app/src/main/java/com/example/efficomobile/ui/theme/Screen/SignUp.kt
+++ app/src/main/java/com/example/efficomobile/ui/theme/Screen/SignUp.kt
@@ -8,47 +8,70 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import androidx.navigation.compose.rememberNavController
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignUp() {
 
-    val emailTextFieldValue = remember {
-        mutableStateOf(TextFieldValue(""))
-    }
-    val passwordTextFieldValue = remember {
-        mutableStateOf(TextFieldValue(""))
-    }
+    val navController = rememberNavController()
+
+    var emailTextFieldValue by remember { mutableStateOf(TextFieldValue("")) }
+    var passwordTextFieldValue by remember { mutableStateOf(TextFieldValue("")) }
 
     Scaffold() {
         Column(modifier = Modifier.padding(it)) {
-
             TextField(
-                value = emailTextFieldValue.value,
-                onValueChange = { emailTextFieldValue.value = it },
+                value = emailTextFieldValue,
+                onValueChange = { emailTextFieldValue = it },
                 label = { Text(text = "Email") },
                 maxLines = 1,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .padding(bottom = 16.dp)
             )
 
             TextField(
-                value = passwordTextFieldValue.value,
-                onValueChange = { passwordTextFieldValue.value = it },
+                value = passwordTextFieldValue,
+                onValueChange = { passwordTextFieldValue = it },
                 label = { Text(text = "Mot de passe") },
                 visualTransformation = PasswordVisualTransformation(),
                 maxLines = 1,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .padding(bottom = 16.dp)
             )
 
             Button(
                 onClick = {
-                    // Envoyer la requête d'inscription à Firebase avec les informations d'identification
-                    val email = emailTextFieldValue.value.text
-                    val password = passwordTextFieldValue.value.text
+                    val email = emailTextFieldValue.text
+                    val password = passwordTextFieldValue.text
 
-
+                    if (email.isNotEmpty() && password.isNotEmpty()) {
+                        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    // L'utilisateur s'est connecté avec succès
+                                    navController.navigate("Home") // Remplacez "Home" par l'ID de votre destination "Home" dans nav_graph.xml
+                                } else {
+                                    try {
+                                        throw task.exception!!
+                                    } catch (e: FirebaseAuthInvalidUserException) {
+                                        // L'utilisateur n'existe pas
+                                    } catch (e: FirebaseAuthInvalidCredentialsException) {
+                                        // Mot de passe incorrect
+                                    } catch (e: Exception) {
+                                        // Autres erreurs
+                                    }
+                                }
+                            }
+                    } else {
+                        // afficher un message demandant de remplir les champs vides
+                    }
                 },
                 modifier = Modifier.align(Alignment.End)
             ) {
